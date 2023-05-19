095eb3f4088404bec14bf6e7bd26ca37c5b26e60
preseah <129074030+preseah@users.noreply.github.com>
2023-05-19 14:10:13 +0900

    Add files via upload

---
+++ get_postlist.py
@@ -0,0 +1,31 @@
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+import time
+
+options = Options()
+options.add_argument('--headless')
+options.add_argument('--disable-gpu')
+driver = webdriver.Chrome(options=options)
+
+def get_parsed_text():
+    driver.get('https://m.blog.naver.com/stageinfo/?categoryNo=0&listStyle=post')    
+    time.sleep(2)
+    page_source = driver.page_source
+    soup = BeautifulSoup(page_source, 'html.parser')
+    # ul_tags = soup.find_all('ul', {'class': ['list__aJqVF', 'list__bcW5n']})
+    ul_tags = soup.find_all('div', {'class': ['content__zHIn1']})
+    span_texts = []
+    for ul_tag in ul_tags:
+        span_tags = ul_tag.find_all('strong', {'class': 'title__yuIvy'})
+        for span_tag in span_tags:
+            span_texts.append(span_tag.text)
+    return span_texts
+
+if __name__ == '__main__':
+    span_texts = get_parsed_text()
+    for span_text in span_texts:
+        print(span_text)
+
+
