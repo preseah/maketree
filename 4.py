b2dc32f7e65d4c6aa3bb320133d6561c7d5e76b2
tsstt <st45783@naver.com>
2023-05-22 12:38:47 +0900

    ver1 commit

---
+++ 4.py
@@ -0,0 +1,81 @@
+import random
+import time
+from selenium import webdriver 
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from requests import get
+from fake_useragent import UserAgent
+from stem import Signal
+from stem.control import Controller
+from bs4 import BeautifulSoup
+import datetime
+import random
+import linecache
+
+USED_IPS = set()
+MAX_IPS = 1000
+
+def get_tor_ip():
+    with Controller.from_port(port = 9051) as controller:
+        controller.authenticate(password="1234qwer")
+        controller.signal(Signal.NEWNYM)
+        time.sleep(controller.get_newnym_wait())
+        with get("https://ident.me", proxies={"http": "socks5://localhost:9050", "https": "socks5://localhost:9050"}) as response:
+            return response.text.strip()
+
+def reset_used_ips():
+    global USED_IPS
+    USED_IPS = set()
+
+def open_url():    
+    hostname="socks5://127.0.0.1"
+    port="9050"
+    options = webdriver.ChromeOptions()
+    options.add_argument('--proxy-server=%s' % hostname + ":" + port) 
+    user_agent = UserAgent().random
+    options.add_argument(f'user-agent={user_agent}')
+    options.add_argument('headless')
+    service = Service('/usr/lib64/chromium-browser/chromedriver')
+    driver = webdriver.Chrome(service=service, options=options)
+    line_number = random.randint(1, len(open('post_name.txt',encoding='utf-8').readlines()))
+    post_name = linecache.getline('post_name.txt', line_number).strip()
+    print('📢 < '+ post_name +' >search naver')
+    driver.get('https://search.naver.com/search.naver?ie=UTF-8&query=%22'+post_name+'%22&sm=chr_hty')
+    wait = WebDriverWait(driver, 10)
+    wait.until(EC.presence_of_element_located((By.TAG_NAME, 'body')))
+    print('📢 search naver complete')
+    driver.find_element(By.CSS_SELECTOR, 'a[href*="blog.naver.com/stageinfo/"]').click()
+    driver.switch_to.window(driver.window_handles[-1])
+    wait.until(EC.presence_of_element_located((By.TAG_NAME, 'body')))
+    # post_element=driver.find_element(By.CSS_SELECTOR, 'a[href*="blog.naver.com/stageinfo/"]')
+    # post_url = post_element.get_attribute('href')
+    # print('📢 ' + post_url + ' loading')
+    # driver.get(post_url)
+    # wait.until(EC.presence_of_element_located((By.TAG_NAME, 'body')))
+    print('📢 post load complete')
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    print('📢 scrooll down')
+    wait=random.randint(60, 120)
+    print('📢 wating : '+str(wait)+'sec')
+    time.sleep(wait)
+    driver.quit()
+
+if __name__=="__main__":
+    while True:
+        ip = get_tor_ip()
+        while ip in USED_IPS:
+            ip = get_tor_ip()
+        USED_IPS.add(ip)
+        print(f"📢 Current IP: {ip}")
+        gmt_now = datetime.datetime.utcnow()
+        kst_now = gmt_now + datetime.timedelta(hours=9)
+        print(kst_now)
+        try:
+            open_url()
+            end=random.randint(10,70)
+            print("휴식시간 :" + str(end))
+            time.sleep(end)
+        except:
+            pass
